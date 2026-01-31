f3db3d27f3186c09fa3712004f030d0d3eccc5c7
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-31 22:03:22 +0530

    version update: 2.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/',(req,res)=>{
-    return res.json({ msg: 'Hello from the server v1' });
+    return res.json({ msg: 'Hello from the server v2' });
 });
 
 app.listen(PORT,()=> console.log(`Server is running on PORT ${PORT}`));
